e7a737f151dd3cbb5a7b0e580d37ca95e52b6ee9
JulieJool <150677323+JulieJool@users.noreply.github.com>
2025-06-30 22:48:39 +0300

    Create bucket.tf

---
+++ terr-setup/bucket.tf
@@ -0,0 +1,9 @@
+# Создание бакета для хранения стейт-файла
+resource "yandex_storage_bucket" "terraform-state-bucket" {
+  bucket     = "terraform-state-bucket-of-julia-teplov"
+  access_key = yandex_iam_service_account_static_access_key.terr-sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.terr-sa-static-key.secret_key
+  acl    = "private"
+  force_destroy = true
+  depends_on = [yandex_resourcemanager_folder_iam_member.editor-sa]
+}
