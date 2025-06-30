f1fe0cdd0afc3e67a75b4171ea6bbc423bce8b6e
JulieJool <150677323+JulieJool@users.noreply.github.com>
2025-07-01 00:36:19 +0300

    Update sa.tf

--- terr-setup/sa.tf
+++ terr-setup/sa.tf
@@ -29,8 +29,13 @@ resource "yandex_resourcemanager_folder_iam_member" "alb-admin-sa" {
   depends_on = [yandex_iam_service_account.terr-sa]
 }
 
-# Создание статического ключа доступа для сервисного аккаунта
+/*# Создание статического ключа доступа для сервисного аккаунта
 resource "yandex_iam_service_account_static_access_key" "terr-sa-static-key" {
   service_account_id = yandex_iam_service_account.terr-sa.id
   description        = "Static access key for terr-sa"
+}*/
+resource "yandex_iam_service_account_key" "sa_key" {
+  service_account_id = yandex_iam_service_account.terr-sa.id
+  description        = "Key for Terraform provider auth"
+  key_algorithm      = "RSA_4096"  # или другой алгоритм
 }
