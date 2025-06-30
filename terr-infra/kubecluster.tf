84d4e061caade8a39d4fb75da24d2deb89c51130
JulieJool <150677323+JulieJool@users.noreply.github.com>
2025-06-30 22:52:16 +0300

    Create kubecluster.tf

---
+++ terr-infra/kubecluster.tf
@@ -0,0 +1,34 @@
+# Создание кластера Kubernetes
+resource "yandex_kubernetes_cluster" "k8s_cluster" {
+  name        = "my-k8s-cluster"
+  description = "Regional Kubernetes cluster"
+  network_id  = yandex_vpc_network.network.id
+
+  master {
+    version = "1.31"
+    regional {
+      region = "ru-central1"
+
+      location {
+        zone      = yandex_vpc_subnet.subnet["0"].zone
+        subnet_id = yandex_vpc_subnet.subnet["0"].id
+      }
+
+      location {
+        zone      = yandex_vpc_subnet.subnet["1"].zone
+        subnet_id = yandex_vpc_subnet.subnet["1"].id
+      }
+
+      location {
+        zone      = yandex_vpc_subnet.subnet["2"].zone
+        subnet_id = yandex_vpc_subnet.subnet["2"].id
+      }
+    }
+
+    public_ip = true # Обеспечивает доступ к мастеру из интернета
+  }
+
+  service_account_id      = jsondecode(var.service_account_key_json).service_account_id
+  node_service_account_id = jsondecode(var.service_account_key_json).service_account_id
+  depends_on              = [yandex_vpc_subnet.subnet]
+}
