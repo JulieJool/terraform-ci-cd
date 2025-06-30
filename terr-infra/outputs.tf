9c687af9fee3a99d0cf734a0b2a611b6275e4336
JulieJool <150677323+JulieJool@users.noreply.github.com>
2025-06-30 23:45:06 +0300

    Update outputs.tf

--- terr-infra/outputs.tf
+++ terr-infra/outputs.tf
@@ -3,7 +3,7 @@ data "yandex_kubernetes_node_group" "node_group" {
   node_group_id = yandex_kubernetes_node_group.node_group.id
 }
 
-output "node_ips" {
+/*output "node_ips" {
   description = "IP addresses and other details of the Kubernetes nodes"
   value = [
     for node in data.yandex_kubernetes_node_group.node_group : {
@@ -15,16 +15,16 @@ output "node_ips" {
       external_ip = node.cloud_status.external_ipv4_address # Или другое поле, содержащее внешний IP
     }
   ]
-}
+}*/
 /*# Получение информации о мастер-ноде через data-источник
 data "yandex_kubernetes_cluster" "k8s_cluster" {
   cluster_id = yandex_kubernetes_cluster.k8s_cluster.id
 }*/
 
-output "master_details" {
+/*output "master_details" {
   value = {
     external_endpoint = data.yandex_kubernetes_cluster.k8s_cluster.master[0].endpoints.external_v4_endpoint
     internal_endpoint = data.yandex_kubernetes_cluster.k8s_cluster.master[0].endpoints.internal_v4_endpoint
     locations         = [for loc in data.yandex_kubernetes_cluster.k8s_cluster.master : loc.locations]
   }
-}
+}*/
