8b6fbf85245183ecc2187f68cbbbee085360aeb7
JulieJool <150677323+JulieJool@users.noreply.github.com>
2025-07-01 00:16:05 +0300

    Update providers.tf

--- terr-infra/providers.tf
+++ terr-infra/providers.tf
@@ -41,10 +41,9 @@ data "terraform_remote_state" "setup" {
 }
 
 provider "yandex" {
-  service_account_key_file = jsonencode({
-    id                   = data.terraform_remote_state.setup.outputs.service_account_id
+  service_account_key_file =({
     service_account_id   = data.terraform_remote_state.setup.outputs.service_account_id
-    private_key          = data.terraform_remote_state.setup.outputs.secret_key
+    private_key          =  jsonencode(data.terraform_remote_state.setup.outputs.secret_key)
   })
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
